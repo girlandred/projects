01af5b579cffb7e5c2be8c41740d8d98c77bb60d
Marina <marinka.shita@gmail.com>
2020-12-09 00:40:21 +0200

    all labs done

---
+++ universityprojects/homework7_task1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homework7_task1
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public IEnumerable<string> PhoneNumbers { get; set; }
+        public Person(string name, int age, IEnumerable<string> phoneNumbers)
+        {
+            Name = name;
+            Age = age;
+            PhoneNumbers = phoneNumbers;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> PersonList = new List<Person>
+            {
+                new Person("David", 16, new List<string>() { "0732155750", "0941533395" }),
+                new Person("Anton", 31, new List<string>() { "0732155750", "0941533395" }),
+                new Person("Sasha", 89, new List<string>() { "0952145758", "0941533395" }),
+                new Person("Ihor", 8, new List<string>() { "0732155750", "0941533395" }),
+                new Person("Ilia", 15, new List<string>() { "0732155750", "0941533395" })
+            };
+
+            foreach (var Person in PersonList)
+            {
+                Console.WriteLine("\n Name: " + Person.Name + " " + "Age: " + Person.Age);
+            }
+
+            Console.WriteLine("\n Using the AddRange Method");
+
+            PersonList.AddRange(new List<Person>() {
+                new Person("Bob", 49, new List<string>() { "0732155750", "0941533395" }),
+                new Person("Jake", 18, new List<string>() { "0732155750", "0941533395" })});
+
+            foreach (var Person in PersonList)
+            {
+                Console.WriteLine("\n Name: " + Person.Name + " " + "Age: " + Person.Age);
+            }
+
+            foreach (var item in PersonList)
+            {
+                Console.WriteLine("\n {0} phone numbers:", item.Name);
+                foreach (var number in item.PhoneNumbers)
+                {
+                    Console.WriteLine("\t -{0}", number);
+                }
+            }
+        }
+    }
+}
---
+++ universityprojects/homework7_task3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homework7_task3
+{
+    class Program
+    {
+        public static List<string> strings;
+        static void Main(string[] args)
+        {
+            strings = new List<string>(100);
+            Random random = new Random();
+            string temp;
+
+            for (int i = 0; i < 100; i++)
+            {
+                temp = "";
+                for (int j = 0; j < 4; j++)
+                {
+                    temp += (char)((random.Next() % 26) + 65);
+                }
+
+                strings.Add(temp);
+            }
+
+            Console.WriteLine("Amount of symbols before filtering: {0}", strings.Count);
+
+            while (strings.FindIndex(l => l[0] == 'Z') != -1)
+            {
+                strings.RemoveAt(strings.FindIndex(l => l[0] == 'Z'));
+            }
+
+            Console.WriteLine("Amount of symbols after filtering: {0}", strings.Count);
+
+            strings.Sort();
+            for (int i = 0; i < strings.Count / 2; i++)
+            {
+                temp = strings[i];
+                strings[i] = strings[strings.Count - 1 - i];
+                strings[strings.Count - 1 - i] = temp;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("\nEnter nubmer of page: ");
+                if (!int.TryParse(Console.ReadLine(), out int pageNumber))
+                    break;
+                pageNumber--;
+                DisplayPage(pageNumber);
+            }
+
+            return;
+        }
+        public static void DisplayPage(int pageNum)
+        {
+            if (pageNum * 5 > strings.Count
+                || pageNum < 0)
+            {
+                Console.Write("Error. Enter another page");
+                return;
+            }
+            for (int i = pageNum * 5; !(i >= (pageNum * 5) + 5
+                                        || i >= strings.Count); i++)
+            {
+                Console.Write(strings[i] + "  ");
+            }
+
+            return;
+        }
+    }
+}
